91e393ef8dce8846a83d5b941711cb12fa068d02
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-04-19 01:08:17 +0530

    Created feed component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,16 +4,19 @@ import Feed from "../../Components/Feed/Feed";
 
 const Home = ({ sidebar }) => {
   return (
-    <>
+    <div className="flex">
+      {/* Sidebar */}
       <Sidebar sidebar={sidebar} />
+
+      {/* Feed */}
       <div
-        className={`  ${
-          sidebar ? "" : "largeContainer"
+        className={`bg-[#f9f9f9] flex-1 transition-all duration-300 pr-[2%] pt-5 pb-5 ${
+          sidebar ? "pl-[17%]" : "pl-[7%]"
         }`}
       >
         <Feed />
       </div>
-    </>
+    </div>
   );
 };
 
